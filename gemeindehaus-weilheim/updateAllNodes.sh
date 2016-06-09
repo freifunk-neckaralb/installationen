f591955f31ef1557471818c5d61421edfb842d48
Justin Humm <justin.humm@posteo.de>
2016-06-09 17:11:19 +0200

    ipv6 gemweil unifi2

--- gemeindehaus-weilheim/updateAllNodes.sh
+++ gemeindehaus-weilheim/updateAllNodes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 2OGr fd21:b4dc:4b02:0:46d9:e7ff:feb9:580f
-scp -6 ffs-tue-gemeindehaus-weilheim-unifi-1.sh root@\[fd21:b4dc:4b02:0:46d9:e7ff:fec7:dce5\]:/tmp/
-scp -6 ffs-tue-gemeindehaus-weilheim-unifi-2.sh root@\[\]:/tmp/
 scp -6 ffs-tue-gemeindehaus-weilheim-1043.sh  root@\[fd21:b4dc:4b02:0:c24a:ff:fef6:c0ae\]:/tmp/
+scp -6 ffs-tue-gemeindehaus-weilheim-unifi-1.sh root@\[fd21:b4dc:4b02:0:46d9:e7ff:fec7:dce5\]:/tmp/
+scp -6 ffs-tue-gemeindehaus-weilheim-unifi-2.sh root@\[fd21:b4dc:4b02:0:46d9:e7ff:fec7:e1c7\]:/tmp/
 
