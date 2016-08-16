136f34e3a71c0786513351789d5b3acfed09b83a
Stefan Tzeggai <tzeggai@empirica-systeme.de>
2016-08-16 22:35:21 +0200

    TrafficControl Limit im Uplink gesenkt, also weniger für Freifunk. Hilft hoffentlich, weil der Uplink regelmäßig offline geht...

--- unterkunft-weilheim/ffs-Tue-MS48-1043nd-uplk.sh
+++ unterkunft-weilheim/ffs-Tue-MS48-1043nd-uplk.sh
@@ -10,11 +10,14 @@ uci set gluon-node-info.@location[0].altitude='350'
 uci set gluon-node-info.@location[0].latitude='48.5037770'
 uci set gluon-node-info.@location[0].longitude='9.05386582'
 
+# Sehr stark limitiertes Internetnutzung 
+uci set simple-tc.mesh_vpn='interface'
+uci set simple-tc.mesh_vpn.ifname='mesh-vpn'
 uci set simple-tc.mesh_vpn.enabled='1'
-uci set simple-tc.mesh_vpn.limit_ingress='7000'
-uci set simple-tc.mesh_vpn.limit_egress='1000'
+uci set simple-tc.mesh_vpn.limit_ingress='4500'
+uci set simple-tc.mesh_vpn.limit_egress='700'
 
-uci set network.mesh_wan.mesh_no_rebroadcast='0'
+#uci set network.mesh_wan.mesh_no_rebroadcast='0'
 uci set network.mesh_lan.mesh_no_rebroadcast='1' # Wir machen ja Mesh-On-LAN nach Weilheim
 
 # Per Cron Nachts um 4:15 rebooten
