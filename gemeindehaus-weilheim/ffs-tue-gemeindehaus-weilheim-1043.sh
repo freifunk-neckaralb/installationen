dec033843ef0ea82de286e2e1ec5d922e8762bfb
Stefan Tzeggai <tzeggai@empirica-systeme.de>
2016-09-03 12:25:06 +0200

    Config von Unifi im Kirchturm angepasst, sowie 2 GW-Verbinundgen im Gemeindehaus eingestellt bis ein zweiter Uplink im Mesh bereitsteht.

--- gemeindehaus-weilheim/ffs-tue-gemeindehaus-weilheim-1043.sh
+++ gemeindehaus-weilheim/ffs-tue-gemeindehaus-weilheim-1043.sh
@@ -14,7 +14,7 @@ uci commit
 # Bandbreitenbegrenzung
 uci set simple-tc.mesh_vpn='interface'
 uci set simple-tc.mesh_vpn.ifname='mesh-vpn'
-uci set simple-tc.mesh_vpn.enabled='1' #Mesh over VPN ist an
+uci set simple-tc.mesh_vpn.enabled='0' #Mesh over VPN ist AUS, die nächsten zwei Zeilen sind deshalb unwichtig
 uci set simple-tc.mesh_vpn.limit_ingress='10000' #maximal 10Mbit down
 uci set simple-tc.mesh_vpn.limit_egress='2000'   #maximal 2MBit up
 uci commit
@@ -55,6 +55,9 @@ uci commit
 uci set network.mesh_wan.mesh_no_rebroadcast='1' #WAN off
 uci set network.mesh_lan.mesh_no_rebroadcast='1' #LAN off
 uci commit network
+
+# Zwei GW-Verbindungen erzeugen mehr Rauschen, aber sind robuster bis in diesem Mesh ein 2. Uplink bereitsteht
+uci set fastd.mesh_vpn_backbone.peer_limit=2
 
 wifi
 reboot
