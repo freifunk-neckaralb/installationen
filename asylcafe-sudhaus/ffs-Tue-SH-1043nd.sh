592574b19789e8cd17e3495cc436a460dad81e4b
Stefan Tzeggai <tzeggai@empirica-systeme.de>
2016-10-27 14:16:03 +0200

    Der Uplink im Sudhaus bau jetzt Verb zu 2 gateways auf und mesht wieder on WLAN

--- asylcafe-sudhaus/ffs-Tue-SH-1043nd.sh
+++ asylcafe-sudhaus/ffs-Tue-SH-1043nd.sh
@@ -19,15 +19,20 @@ uci set gluon-node-info.@location[0].altitude='350'
 uci set gluon-node-info.@location[0].latitude='48.500094310'
 uci set gluon-node-info.@location[0].longitude='9.065316617'
 
-uci set simple-tc.mesh_vpn.enabled='1'
-uci set simple-tc.mesh_vpn.limit_ingress='7000'
-uci set simple-tc.mesh_vpn.limit_egress='1000'
+uci set simple-tc.mesh_vpn.enabled='0'
+#uci set simple-tc.mesh_vpn.limit_ingress='7000'
+#uci set simple-tc.mesh_vpn.limit_egress='1000'
+
+# 2 Gateways verbinden damit robuster..
+uci set fastd.mesh_vpn_backbone.peer_limit=2
 
 uci set network.mesh_wan.mesh_no_rebroadcast='0'
 uci set network.mesh_lan.mesh_no_rebroadcast='0'
 
+# Das Mesh-On-Lan KLAPPT NICHT (ST, 27.10.16)
 uci set network.mesh_lan.auto=1 # Mesh-On-LAN zur Pico
-uci set wireless.ibss_radio0.disabled=1 
+# Das Mesh-On-Lan KLAPPT NICHT (ST, 27.10.16), deshalb darf das hier nicht disabled sein!!!
+uci set wireless.ibss_radio0.disabled=0
 
 # privates wlan, Bitte PSK vor Anwendung durch echten Key austauschen
 uci set wireless.wan_radio0=wifi-iface
